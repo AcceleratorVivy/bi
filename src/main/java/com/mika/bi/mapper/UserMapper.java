07d3708c2e6b53f55df3c08bf31d866de1e19273
sensei <123123@123>
2024-03-08 17:22:13 +0800

    first init

---
+++ src/main/java/com/mika/bi/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.mika.bi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mika.bi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
